503a175a9bb558fa6dbde1d78a581f9494576066
ElChickenSpicy <115719749@umail.ucc.ie>
2021-02-17 14:36:00 +0000

    Skeleton layout for Home Page - Desktop

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,7 +4,92 @@ import './App.css';
 export class App extends React.Component {
   render() {
     return (
-      <h1>Hello World</h1>
+      <div className="top-container">
+
+        <nav>
+          <input className="searchbar" placeholder="Search..."></input>
+          <section className="subreddit-container">
+            <header className="subreddit-title">Subreddits</header>
+
+          </section>
+        </nav>
+
+        <main>
+          <header className="main-header">
+            <h2>Current Page</h2>
+            <div>
+              <img alt="icon" />
+              <h1>Sean's Reddit App</h1>
+            </div>
+          </header>
+
+          <article className="reddit-post">
+            <div className="post-flex-item sub">
+              <img alt="icon"/>
+              <h3>r/something</h3>
+            </div>
+            <div className="post-flex-item content">
+              <h1 className="content-title">Title of Post</h1>
+              <p className="content-media">Content of the post</p>
+            </div>
+            <div className="post-flex-item options">
+              <button>Upvote</button>
+              <button>Downvote</button>
+              <button>Comments</button>
+            </div>
+          </article>
+
+          <article className="reddit-post">
+            <div className="post-flex-item sub">
+              <img alt="icon"/>
+              <h3>r/something</h3>
+            </div>
+            <div className="post-flex-item content">
+              <h1 className="content-title">Title of Post</h1>
+              <p className="content-media">Content of the post</p>
+            </div>
+            <div className="post-flex-item options">
+              <button>Upvote</button>
+              <button>Downvote</button>
+              <button>Comments</button>
+            </div>
+          </article>
+
+          <article className="reddit-post">
+            <div className="post-flex-item sub">
+              <img alt="icon"/>
+              <h3>r/something</h3>
+            </div>
+            <div className="post-flex-item content">
+              <h1 className="content-title">Title of Post</h1>
+              <p className="content-media">Content of the post</p>
+            </div>
+            <div className="post-flex-item options">
+              <button>Upvote</button>
+              <button>Downvote</button>
+              <button>Comments</button>
+            </div>
+          </article>
+
+          <article className="reddit-post">
+            <div className="post-flex-item sub">
+              <img alt="icon"/>
+              <h3>r/something</h3>
+            </div>
+            <div className="post-flex-item content">
+              <h1 className="content-title">Title of Post</h1>
+              <p className="content-media">Content of the post</p>
+            </div>
+            <div className="post-flex-item options">
+              <button>Upvote</button>
+              <button>Downvote</button>
+              <button>Comments</button>
+            </div>
+          </article>
+
+        </main>
+
+      </div>
     )
   }
 };
