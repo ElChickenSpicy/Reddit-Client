31dcde29f6265016f606caead0400719eb16c8d8
ElChickenSpicy <115719749@umail.ucc.ie>
2021-02-18 16:00:15 +0000

    Basic tests on statice elements. Last test before fetch API functionality

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { App } from './App';
+import { Navbar } from '../Navbar/Navbar';
+import { Main } from '../Main/Main';
 
 //Setup
-let app = shallow(<App />);
+let wrapper = shallow(<App />);
 
 //Test
 describe('APP component as a whole', () => {
-  it('render the App Component correctly', () => {   
-    expect(app)
+  it('renders the App Component correctly', () => {   
+    expect(wrapper)
     .toMatchSnapshot();
+  });
+  it('checks that the Navbar section exists', () => {
+    const navbar = wrapper.find(Navbar);
+    expect(navbar.exists())
+    .toBe(true);
+  });
+  it('checks that the Main section exists', () => {
+    const main = wrapper.find(Main);
+    expect(main.exists())
+    .toBe(true);
   });
 });
